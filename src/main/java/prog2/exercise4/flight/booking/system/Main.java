5d577f7264ade29c252f6d1e29bcc19a8843976e
Kola-sleeping <126126005+Kola-sleeping@users.noreply.github.com>
2023-03-13 21:25:32 +0800

    Update Main.java

--- src/main/java/prog2/exercise4/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise4/flight/booking/system/Main.java
@@ -1,14 +1,14 @@
 package prog2.exercise4.flight.booking.system;
 
-/**
- * Hello world!
- *
- */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        
-        
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter passenger's full name: ");
+        String passengerFullName = scanner.nextLine();
+        FlightBooking flightBooking = new FlightBooking(passengerFullName);
+        flightBooking.showMessage();
     }
 }
+
